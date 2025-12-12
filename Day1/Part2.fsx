2b3338a3ec75236c1f3c12ee00554d298acfec43
dungarDot <65919488+dungarDot@users.noreply.github.com>
2025-12-12 14:19:57 -0800

    Correct answer.....we've got all this RAM anyways right?

--- Day1/Part2.fsx
+++ Day1/Part2.fsx
@@ -19,12 +19,14 @@ module Operation =
 type Dial = {
     Position: decimal
     ZerosPassed : int
+    Log: string array
 }
 
 module Dial =
     let Create() = 
         {   Position = 50m
-            ZerosPassed = 0 }
+            ZerosPassed = 0 
+            Log = [||] }
 
     let basicLog dial spinValue newPosition finalZero =
             printfn $"CurrentZeros: {dial.ZerosPassed} | StartingPosition {dial.Position} | SpinValue: {spinValue} | NewPosition: {newPosition} | FinalZero: {finalZero} | FinalCalc : {dial.ZerosPassed + finalZero}"  
@@ -68,10 +70,63 @@ module Dial =
 
 let dial = Dial.Create()
 
+// files  
+// // |> Array.take 10
+// |> Array.Parallel.map(fun line -> 
+//     let direction = line.Substring(0,1)
+//     let value = decimal (line.Substring 1)
+//     Operation.Parse(direction, value)) 
+// |> Array.fold Dial.Spin dial
+
+module BruteFORCE =
+    type  BruteDial = {
+        Position: int
+        Zeros: int
+    }
+
+    type BruteOperation =
+    | L of int
+    | R of int
+
+    let BruteParse (direction: string, value: int) =
+        // printfn "Parsing operation: %s%d" direction value
+        match direction with
+        | "L" -> L value
+        | "R" -> R value
+        | _ -> failwith "Unexpected direction"
+
+    let Create() = 
+        {   Position = 50
+            Zeros = 0 }
+
+    let turnLeft dial = 
+        let newPosition = dial.Position - 1
+        match newPosition with 
+        | -1 -> { dial with Position = 99 }
+        | 0 -> { dial with Position = newPosition; Zeros = dial.Zeros + 1 }
+        | _ -> { dial with Position = newPosition }
+
+    let turnRight dial = 
+        let newPosition = dial.Position + 1
+        match newPosition with 
+        | 100 -> { dial with Position = 0; Zeros = dial.Zeros + 1  }
+        | _ -> { dial with Position = newPosition }
+
+    let Spin dial operation   =
+        match operation with
+        | L value -> 
+            [1 .. value]
+            |> List.fold (fun acc _ -> turnLeft acc) dial
+        | R value -> 
+            [1 .. value]
+            |> List.fold (fun acc _ -> turnRight acc) dial
+
+let bruteDial = BruteFORCE.Create()
+
 files  
 // |> Array.take 10
 |> Array.Parallel.map(fun line -> 
     let direction = line.Substring(0,1)
     let value = decimal (line.Substring 1)
-    Operation.Parse(direction, value)) 
-|> Array.fold Dial.Spin dial
+    BruteFORCE.BruteParse(direction, int value)) 
+|> Array.fold BruteFORCE.Spin bruteDial
