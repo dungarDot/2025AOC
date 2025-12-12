91b90cfa186622a75e20ac40552bea179556e92d
dungarDot <65919488+dungarDot@users.noreply.github.com>
2025-12-12 14:56:15 -0800

    id range function

--- Day2/Part1.fsx
+++ Day2/Part1.fsx
@@ -6,8 +6,16 @@ let example =
     File.ReadAllLines(__SOURCE_DIRECTORY__ + "/example.txt") 
     |> Array.head
     |> ranges
-let data = 
+let input = 
     File.ReadAllLines (__SOURCE_DIRECTORY__ + "/input.txt") 
     |> Array.head 
     |> ranges
 
+let generateIDRanges (range:string) =
+    let startID, endID = 
+        range.Split "-" 
+        |> fun arr -> arr.[0] |> int, arr.[1] |> int
+    [startID..endID]
+
+example
+|> Array.Parallel.map generateIDRanges
